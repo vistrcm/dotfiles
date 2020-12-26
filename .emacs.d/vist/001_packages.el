e8b4f848ae8f4364eaa9d5e59f254e1be9069831
Stanislav Vitko <stas@vitko.me>
2020-12-26 10:51:01 -0800

    emacs. paredit on gui

--- .emacs.d/vist/001_packages.el
+++ .emacs.d/vist/001_packages.el
@@ -70,23 +70,25 @@
   (smex-initialize)
   :bind ("M-x" . smex))
 
-;; ;; Better handling of paranthesis when writing Lisps.
-;;(use-package paredit
-;;   :ensure t
-;;   :init
-;;   (add-hook 'clojure-mode-hook #'enable-paredit-mode)
-;;   (add-hook 'cider-repl-mode-hook #'enable-paredit-mode)
-;;   (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-;;   (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-;;   (add-hook 'ielm-mode-hook #'enable-paredit-mode)
-;;   (add-hook 'lisp-mode-hook #'enable-paredit-mode)
-;;   (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-;;   (add-hook 'scheme-mode-hook #'enable-paredit-mode)
-;;   :config
-;;   (show-paren-mode t)
-;;   :bind (("M-[" . paredit-wrap-square)
-;;          ("M-{" . paredit-wrap-curly))
-;;   :diminish nil)
+;; Better handling of paranthesis when writing Lisps.
+;; only on mac UI
+(if window-system
+(use-package paredit
+  :ensure t
+  :init
+  (add-hook 'clojure-mode-hook #'enable-paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'enable-paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook #'enable-paredit-mode)
+  :config
+  (show-paren-mode t)
+  :bind (("M-[" . paredit-wrap-square)
+         ("M-{" . paredit-wrap-curly))
+  :diminish nil))
 
 ;; better defaults			       
 (use-package better-defaults

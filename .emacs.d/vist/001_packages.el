516035391965ea5a083e2b9f9ccfff1ad55110c4
Stanislav Vitko <stas@vitko.me>
2020-12-25 19:56:38 -0800

    emacs. tromey packages

--- .emacs.d/vist/001_packages.el
+++ .emacs.d/vist/001_packages.el
@@ -1,9 +1,18 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
+
+;; Download the ELPA archive description if needed.
+;; This informs Emacs about the latest versions of all packages, and
+;; makes them available for download.
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 
 (when (not (package-installed-p 'use-package))

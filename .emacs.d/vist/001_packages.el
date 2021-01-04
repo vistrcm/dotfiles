34d55a775dc19f4786bfc66a6fa1e39c98a58d9b
Stanislav Vitko <stas@vitko.me>
2021-01-03 21:56:15 -0800

    emacs. fix indentation

--- .emacs.d/vist/001_packages.el
+++ .emacs.d/vist/001_packages.el
@@ -82,22 +82,22 @@
 ;; only on mac UI
 ;; details on testing: https://www.gnu.org/software/emacs/manual/html_node/elisp/Window-Systems.html
 (if (display-graphic-p)
-(use-package paredit
-  :ensure t
-  :init
-  (add-hook 'clojure-mode-hook #'enable-paredit-mode)
-  (add-hook 'cider-repl-mode-hook #'enable-paredit-mode)
-  (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook #'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook #'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-  (add-hook 'scheme-mode-hook #'enable-paredit-mode)
-  :config
-  (show-paren-mode t)
-  :bind (("M-[" . paredit-wrap-square)
-         ("M-{" . paredit-wrap-curly))
-  :diminish nil))
+    (use-package paredit
+      :ensure t
+      :init
+      (add-hook 'clojure-mode-hook #'enable-paredit-mode)
+      (add-hook 'cider-repl-mode-hook #'enable-paredit-mode)
+      (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+      (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+      (add-hook 'ielm-mode-hook #'enable-paredit-mode)
+      (add-hook 'lisp-mode-hook #'enable-paredit-mode)
+      (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+      (add-hook 'scheme-mode-hook #'enable-paredit-mode)
+      :config
+      (show-paren-mode t)
+      :bind (("M-[" . paredit-wrap-square)
+             ("M-{" . paredit-wrap-curly))
+      :diminish nil))
 
 ;; better defaults			       
 (use-package better-defaults

5bcb496213617561f77cca2bb3d449e6f7975f67
Stanislav Vitko <stas@vitko.me>
2021-01-31 11:19:09 -0800

    more plugins to .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,4 @@
 {:user {:plugins [[jonase/eastwood "0.3.13"]
-                  [lein-kibit "0.1.8"]] }}
+                  [lein-kibit "0.1.8"]
+                  [lein-cljfmt "0.7.0"]
+                  [lein-bikeshed "0.5.2"]] }}

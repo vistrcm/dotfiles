5653f057dcf47bee66a165bcaf64ea0f7e8c8d5e
Stanislav Vitko <stas@vitko.me>
2025-02-07 17:45:50 +0100

    fix font for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.color_scheme = 'Nord (Gogh)'
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.font_size = 18.0
 config.enable_scroll_bar = true
 config.window_decorations = "RESIZE"

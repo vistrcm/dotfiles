90f5a826c6bfa133c4baa7bbc7a59ff43bfb267a
Stanislav Vitko <stas@vitko.me>
2025-05-02 12:40:09 -0700

    update terminal configs

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,8 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = 'Nord (Gogh)'
+
+config.color_scheme = 'Catppuccin Frappe'
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.font_size = 18.0
 config.enable_scroll_bar = true
